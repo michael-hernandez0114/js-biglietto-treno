34ba9766e5de47e44d80eb2d22e14279ffa228a5
Michael Hernandez <gammabeta010@gmail.com>
2020-02-18 16:30:23 +0100

    Finito

--- js/mainscript.js
+++ js/mainscript.js
@@ -5,19 +5,25 @@ const scontoMinore = .20;
 const scontoAnziano = .40;
 const prezzoPerKM = .21;
 
-distanzia = prompt('Ciao! Da quanta distanzia vuole andare?');
+distanzia = prompt('Ciao! Da quanta distanza vuole andare?');
 eta = prompt('E la sua eta?');
 
 if (eta < 18) {
     prezzoFinale = (distanzia * prezzoPerKM) * (1 - scontoMinore);
 }
 else if (eta >= 18 && eta < 65) {
-    prezzoFinale = (distanzia * prezzoPerKM);
+    prezzoFinale = distanzia * prezzoPerKM;
 }
 else {
     prezzoFinale = (distanzia * prezzoPerKM) * (1 - scontoAnziano);
 }
 
-document.getElementById('distance').innerHTML = distanzia;
-document.getElementById('age').innerHTML = eta;
-document.getElementById('total-price').innerHTML = prezzoFinale;
+if (prezzoFinale < 1) {
+    prezzoFinale = Math.ceil(prezzoFinale);
+}
+
+
+document.getElementById('distanza').innerHTML = distanzia + 'km';
+document.getElementById('eta').innerHTML = eta;
+document.getElementById('prezzo-finale').innerHTML = prezzoFinale + '\u20ac';
+document.getElementById('prezzo-finale').setAttribute('class','border-left border-top red');
